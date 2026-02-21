0a6a88089a5eebee3d37418488982c1327957cf1
HectorTries <hswheatley@gmail.com>
2026-02-21 17:33:43 +0000

    Fix UI components syntax

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -2,8 +2,8 @@
  * UI Label component
  */
 
-import * as React from 'react';
-import { cn } from '@/lib/utils';
+import * as React from "react";
+import { cn } from "@/lib/utils";
 
 const Label = React.forwardRef<
   HTMLLabelElement,
@@ -12,12 +12,12 @@ const Label = React.forwardRef<
   <label
     ref={ref}
     className={cn(
-      'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+      "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
       className
     )}
     {...props}
   />
 ));
-Label.displayName = 'Label';
+Label.displayName = "Label";
 
 export { Label };
