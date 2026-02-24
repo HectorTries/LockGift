c433c97620239d4a14ba439f0c5bb1b39e5e1003
HectorTries <hswheatley@gmail.com>
2026-02-24 23:27:01 +0000

    Fix webpack wasm config

--- next.config.js
+++ next.config.js
@@ -6,8 +6,20 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  experiments: {
-    asyncWebAssembly: true,
+  webpack: (config, { isServer }) => {
+    // Enable WebAssembly
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+      syncWebAssembly: true,
+    };
+    
+    // Handle tiny-secp256k1 WASM
+    config.resolve.alias = {
+      ...config.resolve.alias,
+    };
+    
+    return config;
   },
 };
 
